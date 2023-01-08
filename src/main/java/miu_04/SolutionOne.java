7bd6ea2217d11e50f1155fe6ee8c35beeba182c2
Mubarek Seid <mubareksd@gmail.com>
2023-01-08 15:52:24 +0300

    solution for question 4

---
+++ src/main/java/miu_04/SolutionOne.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package miu_04;
+
+/**
+ *
+ * @author mubareksd
+ */
+public class SolutionOne {
+
+    public int f(int n) {
+        int sign = 1;
+        if (n == 0) {
+            return 0;
+        }
+        if (n < 0) {
+            sign = -1;
+            n = -n;
+        }
+        int reverse = 0;
+        while (n != 0) {
+            reverse = (reverse * 10) + (n % 10);
+            n /= 10;
+        }
+        return sign * reverse;
+    }
+}
