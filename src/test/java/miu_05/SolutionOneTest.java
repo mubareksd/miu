0e6acf160eeaa3d6c6ab665d0e4b8f2d45f1e90c
Mubarek Seid <mubareksd@gmail.com>
2023-01-08 15:54:17 +0300

    test for question 5

---
+++ src/test/java/miu_05/SolutionOneTest.java
@@ -0,0 +1,89 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package miu_05;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author mubareksd
+ */
+public class SolutionOneTest {
+    
+    SolutionOne solution = new SolutionOne();
+    
+    @Test
+    public void testOne() {
+        int[] first = {1, 8, 3, 2}, second = {4, 2, 6, 1};
+        int[] expected = {1, 2};
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testTwo() {
+        int[] first = {1, 8, 3, 2, 6}, second = {2, 6, 1};
+        int[] expected = {2, 6, 1};
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testThree() {
+        int[] first = {1, 3, 7, 9}, second = {7, 1, 9, 3};
+        int[] expected = {1, 3, 7, 9};
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testFour() {
+        int[] first = {1, 2}, second = {3, 4};
+        int[] expected = {};
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testFive() {
+        int[] first = {}, second = {1, 2, 3};
+        int[] expected = {};
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testSix() {
+        int[] first = {1, 2}, second = {};
+        int[] expected = {};
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testSeven() {
+        int[] first = {1, 2}, second = null;
+        int[] expected = null;
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testEight() {
+        int[] first = null, second = {};
+        int[] expected = null;
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+    
+    @Test
+    public void testNine() {
+        int[] first = null, second = null;
+        int[] expected = null;
+        assertArrayEquals(expected, solution.f(first, second));
+        
+    }
+}
