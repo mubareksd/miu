2495c18c24755ceeaa0434047f3675a7a9fe43c7
Mubarek Seid <mubareksd@gmail.com>
2023-01-10 14:31:50 +0300

    update

--- src/main/java/miu_14/SolutionOne.java
+++ src/main/java/miu_14/SolutionOne.java
@@ -4,6 +4,7 @@ public class SolutionOne {
 
     static int isMadhavArray(int[] a) {
         Boolean flag = false;
+        int ret = 1;
         for (int n = 0; n < a.length; n++) {
             if (n * (n + 1) / 2 == a.length) {
                 flag = true;
@@ -11,22 +12,25 @@ public class SolutionOne {
             }
         }
 
-        if (!flag)
-            return 0;
-
-        int sum;
-        int counter = 0;
-        for (int i = 0; i < a.length; i = i + counter) {
-            sum = 0;
-            for (int j = 1; j <= i + counter; j++) {
-
+        if (flag) {
+            int sum = 0;
+            int counter = 0;
+            for (int i = 0; i < a.length; i = i + counter) {
+                sum = 0;
+                for (int j = i; j <= i + counter; j++) {
+                    sum += a[j];
+                }
+                if (sum != a[0]) {
+                    ret = 0;
+                    break;
+                }
+                counter++;
             }
-            if (sum != a[0]) {
-                return 0;
-            }
-            counter++;
+        } else {
+            ret = 0;
         }
 
-        return 1;
+        return ret;
+
     }
 }
