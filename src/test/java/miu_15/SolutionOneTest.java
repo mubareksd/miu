580cfa048e59cd931d26362cdef99a72ed27b23f
Mubarek Seid <mubareksd@gmail.com>
2023-01-10 13:20:37 +0300

    question 15

---
+++ src/test/java/miu_15/SolutionOneTest.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package miu_15;
+
+import static miu_15.SolutionOne.isInertial;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author mubareksd
+ */
+public class SolutionOneTest {
+
+    @Test
+    public void testOne() {
+        int[] a = { 1 };
+        int expected = 0;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testTwo() {
+        int[] a = { 2 };
+        int expected = 0;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testThree() {
+        int[] a = { 1, 2, 3, 4 };
+        int expected = 0;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testFour() {
+        int[] a = { 1, 1, 1, 1, 1, 1, 2 };
+        int expected = 1;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testFive() {
+        int[] a = { 2, 12, 4, 6, 8, 11 };
+        int expected = 1;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testSix() {
+        int[] a = { 2, 12, 12, 4, 6, 8, 11 };
+        int expected = 1;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testSeven() {
+        int[] a = { -2, -4, -6, -8, -11 };
+        int expected = 0;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testEight() {
+        int[] a = { 2, 3, 5, 7 };
+        int expected = 0;
+        assertEquals(expected, isInertial(a));
+    }
+
+    @Test
+    public void testNine() {
+        int[] a = { 2, 4, 6, 8, 10 };
+        int expected = 0;
+        assertEquals(expected, isInertial(a));
+    }
+}
