bc2eca022ffc11ad96bfa6aaa45463e8039c7d90
RobelRH <robel.redda28@gmail.com>
2023-01-16 21:07:12 +0300

    isTriangular, isMercurial, is235Array

---
+++ src/test/java/miu_55/TestA.java
@@ -0,0 +1,44 @@
+package miu_55;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestA {
+
+    @Test
+    void TestOne() {
+        int n = 1;
+        int expected = 1;
+        assertEquals(expected, QuestionA.isTriangular(n));
+    }
+
+    @Test
+    void TestTwo() {
+        int n = 3;
+        int expected = 1;
+        assertEquals(expected, QuestionA.isTriangular(n));
+    }
+
+    @Test
+    void TestThree() {
+        int n = 6;
+        int expected = 1;
+        assertEquals(expected, QuestionA.isTriangular(n));
+    }
+
+    @Test
+    void TestFour() {
+        int n = 10;
+        int expected = 1;
+        assertEquals(expected, QuestionA.isTriangular(n));
+    }
+
+    @Test
+    void TestFive() {
+        int n = 8;
+        int expected = 0;
+        assertEquals(expected, QuestionA.isTriangular(n));
+    }
+    
+}
---
+++ src/test/java/miu_55/TestB.java
@@ -0,0 +1,72 @@
+package miu_55;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class TestB {
+    
+    @Test
+    void TestOne() {
+        int[] a = {1, 2, 10, 3, 15, 1, 2, 2};
+        int expected = 0;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test2() {
+        int[] a = {5, 2, 10, 3, 15, 1, 2, 2};
+        int expected = 1;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test3() {
+        int[] a = {1, 2, 10, 3, 15, 16, 2, 2};
+        int expected = 1;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test4() {
+        int[] a = {3, 2, 18, 1, 0, 3, -11, 1, 3};
+        int expected = 0;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test5() {
+        int[] a = {2, 3, 1, 1, 18};
+        int expected = 1;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test6() {
+        int[] a = {8, 2, 1, 1, 18, 3, 5};
+        int expected = 1;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test7() {
+        int[] a = {3, 3, 3, 3, 3, 3};
+        int expected = 1;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test8() {
+        int[] a = {1};
+        int expected = 1;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+    @Test
+    void Test9() {
+        int[] a = {};
+        int expected = 1;
+        assertEquals(expected, QuestionB.isMercurial(a));
+    }
+
+}
