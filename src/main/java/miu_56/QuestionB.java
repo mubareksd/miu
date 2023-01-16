bec8686cd0fcae4f7b6f7c57baba34f506daad68
RobelRH <robel.redda28@gmail.com>
2023-01-16 23:32:55 +0300

    computeHms, isMartian, isPairedN

---
+++ src/main/java/miu_56/QuestionB.java
@@ -0,0 +1,29 @@
+package miu_56;
+
+public class QuestionB {
+    
+    public static int isMartian(int[] a) {
+
+        int numOnes = 0;
+        int numTwos = 0;
+
+        for(int i=0; i < a.length - 1; i++) {
+            if(a[i] == a[i+1]) {
+                return 0;
+            }
+        }
+
+        for(int i=0; i < a.length; i++) {
+            if(a[i] == 1) {
+                numOnes++;
+            }
+            if(a[i] == 2) {
+                numTwos++;
+            }
+        }
+
+        return numOnes > numTwos ? 1 : 0;
+
+    }
+
+}
