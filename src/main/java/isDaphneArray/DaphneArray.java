b737806ad7837dfd985fe8893122cf8fba12ea16
Mubarek Seid <mubareksd@gmail.com>
2023-01-19 15:17:08 +0300

    isDaphneArray

---
+++ src/main/java/isDaphneArray/DaphneArray.java
@@ -0,0 +1,13 @@
+package isDaphneArray;
+
+public class DaphneArray {
+    public static int isDaphneArray(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] == 1 && a[i + 1] % 2 != 0) {
+
+            }
+        }
+
+        return 1;
+    }
+}
---
+++ src/test/java/isDaphneArray/DaphneArrayTest.java
@@ -0,0 +1,9 @@
+package isDaphneArray;
+
+import static isDaphneArray
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class DaphneArrayTest {
+    
+}
