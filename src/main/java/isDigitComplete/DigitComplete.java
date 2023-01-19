6cbf12594a294931eff8c1f32edfa54fb826b1a4
RobelRH <robel.redda28@gmail.com>
2023-01-19 22:56:43 +0300

    digit complete 2nd solution

--- src/main/java/isDigitComplete/DigitComplete.java
+++ src/main/java/isDigitComplete/DigitComplete.java
@@ -1,6 +1,25 @@
 package isDigitComplete;
 
 public class DigitComplete {
+
+    // second solution
+    public static int isDigitComplete2(int[] a) {
+
+        if (a == null || a.length < 10 || a.length > 10)
+        return 0;
+
+        for(int i=0; i < a.length; i++) {
+            for(int j=i+1; j < a.length; j++) {
+                if(a[i] == a[j]) {
+                    return 0;
+                }
+            }
+        }
+
+        return 1;
+
+    }
+
     public static int isDigitComplete(int[] a) {
         if (a == null || a.length < 10 || a.length > 10)
             return 0;
