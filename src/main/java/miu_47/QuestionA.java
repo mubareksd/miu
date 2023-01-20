29d7dfa0e13d262f5dd25053400c5a35a3b13704
Mubarek Seid <mubareksd@gmail.com>
2023-01-20 09:11:40 +0300

    largestAdjacentSum

--- src/main/java/miu_47/QuestionA.java
+++ src/main/java/miu_47/QuestionA.java
@@ -1,24 +1,17 @@
 package miu_47;
 
 public class QuestionA {
-    
+
     public static int largestAdjacentSum(int[] a) {
 
-        if(a.length < 2) {
-            return 0;
+        int sum = Integer.MIN_VALUE;
+
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] + a[i + 1] > sum)
+                sum = a[i] + a[i + 1];
         }
 
-        int largeAdj = a[0] + a[1];
-
-        for(int i=0; i < a.length - 1; i++) {
-            int adjSum = a[i] + a[i+1];
-            if(adjSum > largeAdj) {
-                largeAdj = adjSum;
-            }
-        }
-
-        return largeAdj;
-
+        return sum;
     }
 
 }
